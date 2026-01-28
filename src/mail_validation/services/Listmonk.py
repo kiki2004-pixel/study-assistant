9a913f0d1de59ce3009f404061338a40e9e29cd0
Tawanda Chiteshe <dickenstawanda@gmail.com>
2026-01-28 21:11:34 +0200

    chore: auto fix linter errors

--- src/mail_validation/services/Listmonk.py
+++ src/mail_validation/services/Listmonk.py
@@ -39,14 +39,18 @@ class Listmonk:
         result = self.http_client.get(
             f"/api/subscribers?page={page}&per_page={page_size}&query=subscribers.status = 'enabled'"
         )
-        try: return ListmonkData(**result.raise_for_status().json())
-        except httpx.HTTPError: return ListmonkData(data=ListmonkResults(results=[], total=0))
+        try:
+            return ListmonkData(**result.raise_for_status().json())
+        except httpx.HTTPError:
+            return ListmonkData(data=ListmonkResults(results=[], total=0))
 
     def add_subs_to_blocklist(self, ids: list[int]):
         result = self.http_client.put("/api/subscribers/blocklist", json={"ids": ids})
 
-        try: return BlocklistSubscriberResult(**result.raise_for_status().json())
-        except httpx.HTTPError as e: return BlocklistSubscriberResult(data=False)
+        try:
+            return BlocklistSubscriberResult(**result.raise_for_status().json())
+        except httpx.HTTPError:
+            return BlocklistSubscriberResult(data=False)
 
 
     def subscriber_by_email(self, email: str) -> Subscriber:
@@ -55,9 +59,11 @@ class Listmonk:
         )
         try:
             results = ListmonkData(**result.raise_for_status().json())
-            if len(results.data.results) == 0: return Subscriber(id=0, email=email, status="disabled", created_at="", updated_at="")
+            if len(results.data.results) == 0:
+                return Subscriber(id=0, email=email, status="disabled", created_at="", updated_at="")
             return ListmonkData(**result.json()).data.results[0]
-        except httpx.HTTPError: return Subscriber(id=0, email=email, status="disabled", created_at="", updated_at="")
+        except httpx.HTTPError:
+            return Subscriber(id=0, email=email, status="disabled", created_at="", updated_at="")
 
 
     def unsubscribe_subscriber(self, email: str):
@@ -65,8 +71,10 @@ class Listmonk:
             "/api/subscribers/query/blocklist",
             json={"query": f"subscribers.email = '{email}'"},
         )
-        try: return BlocklistSubscriberResult(**result.raise_for_status().json())
-        except httpx.HTTPError as e: return BlocklistSubscriberResult(data=False)
+        try:
+            return BlocklistSubscriberResult(**result.raise_for_status().json())
+        except httpx.HTTPError:
+            return BlocklistSubscriberResult(data=False)
 
 
     def subscribers_count(self) -> int:
