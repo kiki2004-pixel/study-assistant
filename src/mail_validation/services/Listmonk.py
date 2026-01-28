aeb8e418864d63fb43a0d7d0bcdb4007e914db07
Tawanda Chiteshe <dickenstawanda@gmail.com>
2026-01-28 20:56:24 +0200

    feat: add error handling on lismonk lient

--- src/mail_validation/services/Listmonk.py
+++ src/mail_validation/services/Listmonk.py
@@ -5,6 +5,8 @@ from pydantic import BaseModel
 
 from mail_validation.settings import Settings
 
+class BlocklistSubscriberResult(BaseModel):
+    data: bool
 
 class ListmonkResults(BaseModel):
     results: list[Subscriber]
@@ -37,24 +39,35 @@ class Listmonk:
         result = self.http_client.get(
             f"/api/subscribers?page={page}&per_page={page_size}&query=subscribers.status = 'enabled'"
         )
-        return ListmonkData(**result.json())
+        try: return ListmonkData(**result.raise_for_status().json())
+        except httpx.HTTPError: return ListmonkData(data=ListmonkResults(results=[], total=0))
 
     def add_subs_to_blocklist(self, ids: list[int]):
         result = self.http_client.put("/api/subscribers/blocklist", json={"ids": ids})
-        return result.json()
+
+        try: return BlocklistSubscriberResult(**result.raise_for_status().json())
+        except httpx.HTTPError as e: return BlocklistSubscriberResult(data=False)
+
 
     def subscriber_by_email(self, email: str) -> Subscriber:
         result = self.http_client.get(
             f"/api/subscribers?query=subscribers.email = '{email}'"
         )
-        return ListmonkData(**result.json()).data.results[0]
+        try:
+            results = ListmonkData(**result.raise_for_status().json())
+            if len(results.data.results) == 0: return Subscriber(id=0, email=email, status="disabled", created_at="", updated_at="")
+            return ListmonkData(**result.json()).data.results[0]
+        except httpx.HTTPError: return Subscriber(id=0, email=email, status="disabled", created_at="", updated_at="")
+
 
     def unsubscribe_subscriber(self, email: str):
         result = self.http_client.put(
             "/api/subscribers/query/blocklist",
             json={"query": f"subscribers.email = '{email}'"},
         )
-        return result.json()
+        try: return BlocklistSubscriberResult(**result.raise_for_status().json())
+        except httpx.HTTPError as e: return BlocklistSubscriberResult(data=False)
+
 
     def subscribers_count(self) -> int:
         result = self.subscribers(1, 1)
